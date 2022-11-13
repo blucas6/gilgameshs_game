92f25c10282dd66ca3a075642fc24e7ff408aa67
blucas6 <66273291+blucas6@users.noreply.github.com>
2022-11-12 22:48:42 -0500

    moved character to the right

--- src/figurines/figurine.py
+++ src/figurines/figurine.py
@@ -1,16 +1,76 @@
 import pygame
+from enum import Enum
+from copy import deepcopy
 
 from config import *
 from colors import *
+from utilities import *
+from colors import *
+
+class State(Enum):
+    IDLE = 1
+    MOVING = 2
+    ATTACK = 3
+    ULTI = 4
+    DEATH = 5
 
 class Figurine(pygame.sprite.Sprite):
-    def __init__(self, game, width, height, img, groups):
+    def __init__(self, game, width, height, img, groups, stx, sty):
         pygame.sprite.Sprite.__init__(self, groups)
         self.game = game
         self.width = width
         self.height = height
-        self.image = img
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.blit(img, (0,0), (0,0,self.width, self.height))
+        self.image.set_colorkey(VOID)
         self._layer = FIGURINE_LAYER
         self.rect = self.image.get_rect()
-        self.rect.x = 100
+        self.rect.x = stx
+        self.rect.y = sty
+
+        self.current_pos_board = [0,0]
+        self.board_src = [0,0]
+        self.board_dest = [0,0]
+
+        self.state = State.IDLE
+        self.state = self.ChangeState(State.MOVING)
+
+    def update(self):
+        if self.state == State.IDLE:
+            self.Idle()
+        elif self.state == State.MOVING:
+            self.Moving()
+
+    def ChangeState(self, state):
+        if state != self.state:
+            if state == State.MOVING:
+                self.board_dest = deepcopy(self.current_pos_board)
+                self.board_dest[0] += 1
+                self.board_src = deepcopy(self.current_pos_board)
+            return state
+
+    def move(self):
+        if self.board_dest == self.current_pos_board:
+            self.ChangeState(State.IDLE)
+        else:
+            cx = self.board_dest[0] - self.board_src[0]
+            cy = self.board_dest[1] - self.board_src[1]
+            if cx > 0:
+                self.rect.x += FIGURINE_MOVE_SPEED
+                self.current_pos_board[0] += FIGURINE_MOVE_SPEED / TILESIZE_W
+            if cx < 0:
+                self.rect.x -= FIGURINE_MOVE_SPEED
+                self.current_pos_board[0] -= FIGURINE_MOVE_SPEED / TILESIZE_W
+            if cy > 0:
+                self.rect.y += FIGURINE_MOVE_SPEED
+                self.current_pos_board[1] += FIGURINE_MOVE_SPEED / TILESIZE_H
+            if cy < 0:
+                self.rect.y -= FIGURINE_MOVE_SPEED
+                self.current_pos_board[1] -= FIGURINE_MOVE_SPEED / TILESIZE_H
+
+    def Idle(self):
+        pass
+
+    def Moving(self):
+        pass
 
